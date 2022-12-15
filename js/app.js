ccdfcfea9188efe2d03c449be8fde3890343e0c7
dvd_mcf <dave@techia.com>
2022-12-15 16:14:23 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -81,10 +81,10 @@ WebMidi.enable()
     makeSliders(device);
 
     // (Optional) Create a form to send messages to RNBO inputs
-    makeInportForm(device);
+//    makeInportForm(device);
 
     // (Optional) Attach listeners to outports so you can log messages from the RNBO patcher
-    attachOutports(device);
+//    attachOutports(device);
 
     // (Optional) Load presets, if any
     loadPresets(device, patcher);
